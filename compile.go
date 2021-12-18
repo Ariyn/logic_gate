ec2438f0edb83df645c49cd406eb54dca64e91a6
ariyn <himnowxz@gmail.com>
2021-12-18 23:15:09 +0900

    비어있는 compile.go 파일 추가

--- compile.go
+++ compile.go
@@ -1,1 +1,12 @@
 package logic_gate
+
+/*
+	TODO: compile complex gate into static truth table gate
+		to increase simulating speed.
+	need these things.
+		* feedback loop detection -> loop gates could lead infinite loop
+		* predict total ticks
+		* calculate possible combinations of input
+		* generate truth table
+		* truth table save, load
+*/
